10c971e60576ce8a03d297378852d63a9b03bdb0
bramvgemert <bramvgemert@gmail.com>
2015-09-23 23:07:43 +0300

    update

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-it's a thing
+
+#Lua Binder
+
+ ☑ Run lua from C++
+ ☑ Run C++ function from lua
+ ☐ Bind C++ classes to lua
+ ☐ Bind Lua classes to C++
+ ☐ Scopes (luabind style)
+ ☐ Nested Scopes
+ ☐ More stuff
+ 
--- main.cpp
+++ main.cpp
@@ -2,19 +2,9 @@
 #include <lua52/lauxlib.h>
 #include <lua52/lualib.h>
 #include <iostream>
-#include <cstring>
 
-bool dostring(lua_State* L, const char* str)
-{
-	if (luaL_loadbuffer(L, str, std::strlen(str), str) || lua_pcall(L, 0, 0, 0))
-	{
-		const char* a = lua_tostring(L, -1);
-		std::cout << a << "\n";
-		lua_pop(L, 1);
-		return true;
-	}
-	return false;
-}
+#include <bind/binder.h>
+
 int greet(lua_State* l)
 {
 	std::cout << "C++ is a POTATO \n";
@@ -24,9 +14,8 @@ int main()
 {
 	lua_State* L = luaL_newstate();
 	luaL_openlibs(L);
-	lua_pushcfunction(L, greet);
-    lua_setglobal(L, "greet");
-    dostring(L, "greet()");
+	binder::bindFunction(L,"greet",greet);
+    binder::dostring(L, "greet()");
 	lua_close(L);
 }
 
